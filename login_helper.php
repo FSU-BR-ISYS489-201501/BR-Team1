324b85be3419cc5cb4c1309672156a9fbb98217d
markbowman100 <markbowman100@gmail.com>
2015-12-06 17:20:23 -0500

    Create login_helper.php
    This file contains two functions used in the login process.

---
+++ login_helper.php
@@ -0,0 +1,70 @@
+<?php
+// This page defines two functions used by the login/logout process.
+
+function redirect_user ($page = 'index.php') {
+
+  //Define the URL
+	$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
+	
+	// Remove any trailing slashes:
+	$url = rtrim($url, '/\\');
+	
+	// Add the page:
+	$url .= '/' . $page;
+	
+	// Redirect the user:
+	header("Location: $url");
+	exit(); // Quit the script.
+
+} // End of redirect_user() function.
+
+/* This function validates the form data (the username and password).
+ * If both are present, the database is queried.
+ * The function requires a database connection.
+ * The function returns an array of information, including:
+ * - a TRUE/FALSE variable indicating success
+ * - an array of either errors or the database result
+ */
+function check_login($dbc, $username = '', $pass = '') {
+
+	$errors = array(); // Initialize error array.
+
+	// Validate the email address:
+	if (empty($username)) {
+		$errors[] = 'You forgot to enter your username.';
+	} else {
+		$e = mysqli_real_escape_string($dbc, trim($username));
+	}
+
+	// Validate the password:
+	if (empty($pass)) {
+		$errors[] = 'You forgot to enter your password.';
+	} else {
+		$p = mysqli_real_escape_string($dbc, trim($pass));
+	}
+
+	if (empty($errors)) { // If everything's OK.
+
+		// Retrieve the user_id and first_name for that email/password combination:
+		$q = "SELECT user_id, first_name FROM users WHERE username='$e' AND pass='$p'";		
+		$r = @mysqli_query ($dbc, $q); // Run the query.
+		
+		// Check the result:
+		if (mysqli_num_rows($r) == 1) {
+
+			// Fetch the record:
+			$row = mysqli_fetch_array ($r, MYSQLI_ASSOC);
+	
+			// Return true and the record:
+			return array(true, $row);
+			
+		} else { // Not a match!
+			$errors[] = 'Username and password entered do not match those on file.';
+		}
+		
+	} // End of empty($errors) IF.
+	
+	// Return false and the errors:
+	return array(false, $errors);
+
+} // End of check_login() function.
