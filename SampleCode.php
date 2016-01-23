9e9b0666ebe27b695bb7c8c88330f330282e36d7
markbowman100 <markbowman100@gmail.com>
2016-01-23 02:17:00 -0500

    Mark Bowman created SampleCode.php

---
+++ SampleCode.php
@@ -0,0 +1,20 @@
+<?php
+	// Setting a varible and displaying its message
+    $sampleVariable = "This is a variable!";
+	echo($sampleVariable);
+	
+	// Setting a variable and displaying its message
+	$messageForDevelopers = "Write a comment on this page
+		so that Mark knows that you understand how to 
+		push to GitHub.";
+	echo($messageForDevelopers);
+	
+	
+	$understood = true;
+	if ($understood = true) {
+		echo("Understood!");
+	}
+	else {
+		echo("I don't understand!");
+	}
+?>
