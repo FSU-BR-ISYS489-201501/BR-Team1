39315833ff7c46bdc50fcd90c0dbaafbef8b210a
markbowman100 <markbowman100@gmail.com>
2016-01-23 02:18:44 -0500

    Mark Bowman added comments

--- SampleCode.php
+++ SampleCode.php
@@ -3,13 +3,13 @@
     $sampleVariable = "This is a variable!";
 	echo($sampleVariable);
 	
-	// Setting a variable and displaying its message
+	// Giving a message to Software Developers
 	$messageForDevelopers = "Write a comment on this page
 		so that Mark knows that you understand how to 
 		push to GitHub.";
 	echo($messageForDevelopers);
 	
-	
+	// Asking if Software Developers understand
 	$understood = true;
 	if ($understood = true) {
 		echo("Understood!");
