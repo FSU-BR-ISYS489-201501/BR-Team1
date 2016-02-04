c3bb47d46a5181597b573988b1f1d2b425c68ad5
markbowman100 <markbowman100@gmail.com>
2016-02-04 15:37:42 -0500

    Mark Bowman added preliminary code for uploading a file

--- JCI/includes/FileHelper.php
+++ JCI/includes/FileHelper.php
@@ -1,3 +1,24 @@
 <?php
 	// Put code here for functions that will help downloading and uploading files
+	
+	// Mark Bowman: This function will upload a file from the host's computer to the server.
+		
+		if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+			if(isset($_FILES["uploadedFile"])){
+				
+				$uploadedFile = $_FILES["uploadedFile"];
+				
+				// TODO: Mark Bowman use a function to check the file type here
+				if($uploadedFile) {
+				//	move_uploaded_file($uploadedFile, $destination);
+				echo("File uploaded");
+				}
+			}
+			
+			else {
+				echo("You must attach a file before submitting.");
+			}
+		}
+
 ?>
