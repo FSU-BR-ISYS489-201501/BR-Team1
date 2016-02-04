f73490a387ecaa2746be26e1386f599829b79db4
markbowman100 <markbowman100@gmail.com>
2016-02-04 17:41:10 -0500

    Mark Bowman added comments and code to allow a user to directly upload a file to the server

--- JCI/includes/FileHelper.php
+++ JCI/includes/FileHelper.php
@@ -7,17 +7,29 @@
 		
 			if(isset($_FILES["uploadedFile"])){
 				
-				$uploadedFile = $_FILES["uploadedFile"];
+				// Mark bowman: This block contains variables for the browser's temporary name
+				// for the uploaded file and the location it is going to be saved to.
+				$tempUploadedFileName = $_FILES["uploadedFile"]['tmp_name'];
+				$uploadedFileNameSaveLocation = "../uploads/{$_FILES["uploadedFile"]["name"]}";
 				
-				// TODO: Mark Bowman use a function to check the file type here
-				if($uploadedFile) {
-				//	move_uploaded_file($uploadedFile, $destination);
-				echo("File uploaded");
+				
+				//TODO: Allow a user to upload more than one file before submitting.
+				//Mark Bowman: This block checks if a file has been submitted with the HTML
+				// form and then moves it to the final storage location.
+				if(file_exists($tempUploadedFileName)) {
+					
+					if(move_uploaded_file($tempUploadedFileName, 
+						$uploadedFileNameSaveLocation)) {
+							echo '<p> Upload was successful! </p>';
+					}
+					
+					else {
+						echo '<p> Upload was not successful! </p>';
+					}		
 				}
-			}
-			
-			else {
-				echo("You must attach a file before submitting.");
+				else {
+					echo '<p> You must attach a file first. </p>';
+				}
 			}
 		}
 
