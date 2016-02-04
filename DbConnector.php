1e18f6d8f9e0b3bfaa1c6362ac5c3770d3bc5de8
markbowman100 <markbowman100@gmail.com>
2016-02-04 01:54:37 -0500

    Mark Bowman added a comment giving attribution to Larry Ullman

--- DbConnector.php
+++ DbConnector.php
@@ -1,4 +1,6 @@
-<?php # Script 9.2 - mysqli_connect.php
+<?php
+
+// This code was taken from "PHP and MySQL for Dynamic Web Sites", written by Larry Ullman.
 
 // This file contains the database access information. 
 // This file also establishes a connection to MySQL, 
@@ -10,8 +12,10 @@ DEFINE ('DB_PASSWORD', 'rV);R8PriM*8');
 DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'isys489c-BT1-JCI');
 
+
 // Make the connection:
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
+
 
 // Set the encoding...
 mysqli_set_charset($dbc, 'utf8');
