977445670718f7b55cbfcb25a48b626837702be1
markbowman100 <markbowman100@gmail.com>
2016-02-04 17:36:19 -0500

    Mark Bowman created temporay HTML code to upload a file

---
+++ JCI/includes/TempFileUpload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<title>Hello</title>
+<html>
+	<head>
+		
+	</head>
+	<body>
+		
+		<form action = "FileHelper.php" method = "POST" enctype = "multipart/form-data">
+			<input type = "file" name = "uploadedFile" />
+			<input type = "submit" value = "Submit" />
+		</form>
+	</body>
+</html>
