592cf4ab1ef9b89395df91a8881b7946c06931c8
alfadhf <alfadhf@ferris.edu>
2016-02-05 18:32:27 -0500

    Faisal: this is a header file

--- JCI/includes/Header.php
+++ JCI/includes/Header.php
@@ -1,3 +1,27 @@
-<?php
-	// Put code here that will link Register, Login, Logout, etc
-?>
+<!-- By Faisal Alfadhli-->
+<!--Email: alfadhf@ferris.edu-->
+<!--This is my own cod I wrote it 2 years ago for ISYS 288  -->
+<!--02/04/2016 -->
+<!-- header -->
+
+<html>
+<head>
+	<title><?php echo $page_title; ?></title>	
+	<link rel="stylesheet" href="includes/style.css" type="text/css" media="screen" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+<body>
+	<div id="header">
+		<h1>Your Website</h1>
+		
+	</div>
+	<div id="navigation">
+		<ul>
+			<li><a href="index.php">Home Page</a></li>
+			<li><a href="register.php">Register</a></li>
+			<li><a href="login.php">Login</a></li>
+			<li><a href="logout.php">Lougout</a></li>
+			<li><a href="contact.php">Contact Us</a></li>
+		</ul>
+	</div>
+	<div id="content"><!-- Start of the page-specific content. -->
