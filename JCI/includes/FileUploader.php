1e1ae20ba8d8a37d8775150adc7fd6d6470f8ca5
markbowman100 <markbowman100@gmail.com>
2016-02-05 20:39:49 -0500

    Mark Bowman made uploadFile function that will take files from an input div and store at the input location

--- JCI/includes/FileUploader.php
+++ JCI/includes/FileUploader.php
@@ -1,30 +1,48 @@
 <?php
 	// Put code here for functions that will help downloading and uploading files
 	
-	// Mark Bowman: This function will upload a file from the host's computer to the server.
-		
+		// Mark Bowman: This function will upload a file from the host's computer to the server.
+		function uploadFile($divName, $fileStorageLocation) {
+			
+			
+			// Mark Bowman: This block is setting a counter for the number of files and how many have been uploaded.
+			$fileUplaodSuccessCounter = 0;
+			$numberOfFilesUploaded = count($_FILES["$divName"]['tmp_name']);
+			$i = 0;
+			
+			// Mark Bowman: This block is going through all of the uploaded files.
+			for($i; $i < $numberOfFilesUploaded; $i++) {
+	
+			
 				// Mark bowman: This block contains variables for the browser's temporary name
 				// for the uploaded file and the location it is going to be saved to.
+				$tempUploadedFileName = $_FILES["$divName"]['tmp_name'][$i];
+				$uploadedFileNameSaveLocation = $fileStorageLocation . "{$_FILES["$divName"]['name']["$i"]}";
 				
-				for($i = 0; $i < 5; $i++) {
 				
-					$tempUploadedFileName = $_FILES["uploadedFile"]['tmp_name'][$i];
-					$uploadedFileNameSaveLocation = "../uploads/{$_FILES['uploadedFile']['name']["$i"]}";
-					
-					//Mark Bowman: This block checks if a file has been submitted with the HTML
-					// form and then moves it to the final storage location.
-					if(file_exists($tempUploadedFileName)) {
-						if(move_uploaded_file($tempUploadedFileName, 
-							$uploadedFileNameSaveLocation)) {
-								echo '<p> Upload was successful! </p>';
-						}
-						else {
-							echo '<p> Upload was not successful! </p>';
-						}		
+				//Mark Bowman: This block checks if a file has been submitted with the HTML
+				// form and then moves it to the final storage location.
+				if(file_exists($tempUploadedFileName)) {
+					if(move_uploaded_file($tempUploadedFileName, 
+						$uploadedFileNameSaveLocation)) {
+							$fileUplaodSuccessCounter += 1;
 					}
 					else {
+						echo '<p> Upload was not successful! </p>';
+					}		
+				}
+				else {
+					if ($i == 0) {
 						echo '<p> You must attach a file first. </p>';
 					}
+					break;
 				}
+			}
+			
+			if($i == $fileUplaodSuccessCounter) {
+				echo '<p> You did it! </p>';
+			}
+		};
 		
+		uploadFile("uploadedFile", "../uploads/");
 ?>
