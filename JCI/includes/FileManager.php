40f9fb7f9db0d034b8e9fbb4cf29847c191d4ee4
Mark Bowman <markbowman100@gmail.com>
2016-02-06 07:57:22 -0500

    Mark Bowman added a function that checks if the file name exists on the file server

--- JCI/includes/FileManager.php
+++ JCI/includes/FileManager.php
@@ -1,15 +1,29 @@
 <?php
 	// Put code here for functions that will help downloading and uploading files
-	
+		
+		
+		// Mark Bowman: This function checks if the input file name exists on the file server
+		// and returns a file name that doesn't exist on the file server.
+		function checkIfFileExistsOnFileServer($filePath) {
+			$counter = 1;
+			while (file_exists($filePath)) {
+				$counter++;
+			}
+			return $filePath . " ($counter)";
+		};
+		
 		// Mark Bowman: This function will upload a file from the host's computer to the server.
 		function uploadFile($divName, $fileStorageLocation) {
-			
-			
 			// Mark Bowman: This block is setting a counter for the number of 
 			// files and how many have been uploaded.
 			$fileUplaodSuccessCounter = 0;
 			$numberOfFilesUploaded = count($_FILES["$divName"]['tmp_name']);
 			$i = 0;
+			
+			
+			// Mark Bowman: This allows access to the database connection information.
+			include('DbConnector.php');
+			
 			
 			// Mark Bowman: This block is going through all of the uploaded files.
 			for($i; $i < $numberOfFilesUploaded; $i++) {
@@ -19,8 +33,9 @@
 				// for the uploaded file and the location it is going to be saved to.
 				$tempUploadedFileName = $_FILES["$divName"]['tmp_name'][$i];
 				$uploadedFileNameSaveLocation = $fileStorageLocation . "{$_FILES["$divName"]['name']["$i"]}";
-				$insertFileLocationSqlQuery = "INSERT INTO files (content, file_des)
-					VALUES ({$uploadedFileNameSaveLocation}, {$tempUploadedFileName})";
+				$insertFileLocationSqlQuery = "INSERT INTO files (content, file_des, fileid)
+					VALUES ('$tempUploadedFileName', '$uploadedFileNameSaveLocation', '$i')";
+				
 				
 				// Mark Bowman: This block checks if a file has been submitted with the HTML
 				// form and then moves it to the final storage location.
@@ -31,13 +46,13 @@
 							
 							// Mark Bowman: This block performs an SQL query to insert file
 							// location into the database.
+							
 							if (mysqli_query($dbc, $insertFileLocationSqlQuery)) {
 							    echo '<p> Upload was successful. </p>';
 							} 
 							else {
-							    echo '<p> Database Error </p>';
+							    echo '<p> Database Error. </p>';
 							}
-							mysqli_close();
 							$fileUplaodSuccessCounter += 1;
 					}
 					else {
@@ -61,6 +76,9 @@
 				}
 			}
 			
+			
+			// Mark Bowman: This block closes the connection to the database.
+			mysqli_close($dbc);
 		};
 		
 		// SQL function LOAD_FILE(Filename) must be used on server.
