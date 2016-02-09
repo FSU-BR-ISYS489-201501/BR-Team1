85f4e136d3d70c2a76e753303ee8ace67b69d293
Shane Workman <workma18@gmail.com>
2016-02-09 11:39:05 -0500

    Shane: Capitalized the R in Rigister.php

--- JCI/includes/Header.php
+++ JCI/includes/Header.php
@@ -18,7 +18,7 @@
 	<div id="navigation">
 		<ul>
 			<li><a href="index.php">Home Page</a></li>
-			<li><a href="register.php">Register</a></li>
+			<li><a href="Register.php">Register</a></li>
 			<li><a href="login.php">Login</a></li>
 			<li><a href="logout.php">Lougout</a></li>
 			<li><a href="contact.php">Contact Us</a></li>
