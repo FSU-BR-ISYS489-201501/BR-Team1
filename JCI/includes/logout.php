9085722504cd0456cd2008df5e43533e2f2e52d3
Benjamin Brackett <brackeb1@ferris.edu>
2016-02-13 00:51:51 -0500

    Ben Brackett: removed cookies and replaced with sessions

--- JCI/includes/logout.php
+++ JCI/includes/logout.php
@@ -1,15 +1,32 @@
 <?php
-//Author: Benjamin Brackett
-//Email: brackeb1@ferris.edu
-//Date: 02/09/2016
-//This is logout code that I found online 
-//https://github.com/Goatella/Simple-PHP-Login/blob/master/logout.php
-//The actual author of this code is Angela Bradley
+/*********************************************************************************************
+ * Original Author: Benjamin Brackett
+ * Date of origination: 02/09/2016
+ *
+ * Page created for use in the JCI Project.
+ * Project work is done as part of a Capstone class ISYS489: Ferris State University.
+ * Purpose: This is the logout page using sessions
+ * Credit: https://github.com/GobleB/PHP-Login-with-Sessions/blob/master/logout.php
+ *
+ * Revision1.1: 02/12/2016 Author: Benjamin Brackett
+ * Description of change. Changed entire code in order to use sessions
+ ********************************************************************************************/
 
-//
- $past = time() - 100; 
- //this makes the time in the past to destroy the cookie 
- setcookie(ID_my_site, gone, $past); 
- setcookie(Key_my_site, gone, $past); 
- header("Location: login.php"); 
+ 
+require_once('login.php');
+
+
+session_start();
+
+ $username = '';
+if(isset($_SESSION['user'])) {
+	$username = $_SESSION['user'];
+	echo $username . ", you are being logged out."; 
+	$user->logout();
+	header("Refresh: 3; url=index.php");
+	exit();
+} else {
+	header("Location: index.php");
+	exit();
+}
 ?>
