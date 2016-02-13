f78fcd2b31f793b3688d84c3fbca119098bf80ce
markbowman100 <markbowman100@gmail.com>
2016-02-13 12:00:24 -0500

    Mark Bowman added TODOs

--- JCI/SubmitCasesForReview.php
+++ JCI/SubmitCasesForReview.php
@@ -18,80 +18,96 @@
 */
 
 	include ("includes/Header.php");
- 
-
-	// define variables and set to empty values
+	 
+	
+		// define variables and set to empty values
+	
+	//TODO: Set up as an arrary for errors
 	$nameErr; 
 	$emailErr;
 	$fileErr;
+	
 	$name; 
 	$email;
 	$file;
-
+	
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		if (empty($_POST["name"])) {
-     		$nameErr = "Name is required";
-   		} 
-	else {
-		$name = test_input($_POST["name"]);
-    		// check if name only contains letters and whitespace
-     		if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-       			$nameErr = "Only letters and white space allowed"; 
-     		}
-   }
-   
-   if (empty($_POST["email"])) {
- 		// call check email function
-   		CheckEmail();
-   }
-     
-	 
-	if (empty($_POST["filedoc"])) {
-		// call check file type and siez function 
-		CheckFile();
-	   
-	 	 // send email notification 
-		mail($email,"File uploaded",$msg);
+		//TODO: make this check all of the authors names being submitted.
+		//TODO: require 1 author, but allow multiple. if 1 author isn't input, show an error. If 2 authors aren't, don't show an error, but don't include that information in the database.
+		for($i = 0; $i < 4; $i++) {
+			if (empty($_POST["author[$i]"])) {
+				$nameErr = "Name is required";
+			}
+			else {
+			// check if name only contains letters and whitespace
+				if (!preg_match("/^[a-zA-Z ]*$/", $_POST["author[$i]"])) {
+					$nameErr = "Only letters and white space allowed"; 
+				}
+			}
+		} 
+		if (!empty($_POST["email"])) {
+			// call check email function
+			CheckEmail($_POST["email"]);
+		}
+		else {
+		   	$emailErr = "Email field is empty";
+		}
+		     
+		if (!empty($_POST["filedoc"])) {
+			// call check file type and siez function 
+			CheckFile();
+			
+			//TODO: make this check all of the files being uploaded.
+		 	 // send email notification 
+			mail($email,"File uploaded",$msg);
+		}
 	}
-	}
-
-	function test_input($data) {
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
-	}
-?>
-
+	
+	// function testInput($data) {
+		// $data = trim($data);
+		// $data = stripslashes($data);
+		// $data = htmlspecialchars($data);
+		// return $data;
+	// }
+	?>
+	
 	<!-- Create html Form -->
 	<h2>Fill The Form</h2>
-		<p><span class="error">* required field.</span></p>
-
-	<form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
- 
-		E-mail: <input type="text" name="email">
-			<span class="error">* <?php echo $emailErr;?></span>
+	
+	<form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" multiple = "multiple">
+	 
+		Authors: <input type="text" name="author[]">
+		<input type="text" name="author[]">
+		<input type="text" name="author[]">
+		<input type="text" name="author[]">
 		<br><br>
-   		Title: <input type="text" name="name">
-			<span class="error">* <?php echo $nameErr;?></span>
+		Title: <input type="text" name="title">
 		<br><br>
-   		Keyword:<textarea name="comment" rows="5" cols="40"></textarea>
+		Keyword:<input type="text" name="keyword1">
+		Keyword:<input type="text" name="keyword2">
+		Keyword:<input type="text" name="keyword3">
+		Keyword:<input type="text" name="keyword4">
+		Keyword:<input type="text" name="keyword5">
 		<br><br>
-   
-
-		<br /><br />
-	<!-- use for uploading fils -->
-	<label for='uploaded_file'>Select only doc File To Upload:</label>  
-	<br><br>  
-	<input type="file" name="filedoc" id="filedoc" /><br />	<br />
-		<span class="error"><?php echo $fileErr;?></span>    
-	<input type="submit" value="Submit" name="upload_doc" />
-	<br><br>
-
+		   
+		
+		<br><br>
+		<!-- use for uploading fils -->
+		<label for='uploadedFile'>Select only doc File To Upload:</label>  
+		<br><br>  
+		<input type="file" name="filedoc[]" />
+		<input type="file" name="filedoc[]" />
+		<input type="file" name="filedoc[]" />
+		<input type="file" name="filedoc[]" />
+		<input type="file" name="filedoc[]" />
+		<br><br>
+		<input type="submit" value="Submit" name="uploadedFile" />
+		<br><br>
+	
 	</form>
-
-
-<?php
-	include ("includes/Footer.php");
+	
+	
+	<?php
+		include ("includes/Footer.php");
 ?>
 
