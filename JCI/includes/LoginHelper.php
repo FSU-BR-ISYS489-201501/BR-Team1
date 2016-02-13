33aa6cff19096edafe7cec682e1c76460b85e7b6
Benjamin Brackett <brackeb1@ferris.edu>
2016-02-13 00:51:01 -0500

    Ben Brackett: changed a variable

--- JCI/includes/LoginHelper.php
+++ JCI/includes/LoginHelper.php
@@ -1,23 +1,38 @@
 <?php
 	// Put code here for functions that help in the login, logout, and register processes
-	
-	
-	//Author: Ben Brackett 02/05/16
-	//Ben Brackett: This block of code is the function for checking to see if the user is logged in or 
-	//not. I did not create this code myself. It was found off of stackoverflow from
-	//http://stackoverflow.com/questions/18537016/php-check-if-user-is-logged-in-with-a-function
-	//Ben Brackett: This check_login function checks the session login to see if it matches a logged in
-	//session id. If not, then it will return a false, if true, it will return a true. 
-	//If true it will then echo you are in, or just show login screen   
+	/*********************************************************************************************
+ * Original Author: Benjamin Brackett
+ * Date of origination: 02/05/16
+ *
+ * Page created for use in the JCI Project.
+ * Project work is done as part of a Capstone class ISYS489: Ferris State University.
+ * Purpose: Put code here for functions that help in the login, logout, and register processes
+ * Credit: Give any attributation to code used within, not created by you.
+ *
+ * Function:  check_login($myVar, $varTwo)
+ * Purpose: Check if user is logged in
+ * Variable: $_SESSION['user'] - identifies session.
+ * Credit: http://stackoverflow.com/questions/18537016/php-check-if-user-is-logged-in-with-a-function
+ 	
+ *
+ * Function:  is_digits($anotherVar)
+ * Purpose: Checks to see if field only has digits.
+ * Variable: $element - The character in question.
+ * Credit: https://davidwalsh.name/php-validatie-numeric-digits 
+ *
+ * Revision1.1: 02/12/2016 Author: Benjamin Brackett
+ * Description of change: changed $_SESSION['login'] to $_SESSION['user']
+ ********************************************************************************************/
+
 	if(check_login()) {
   echo 'You are in!';
 } else {
     header('Location: login.php');
     exit;
 }
-
+//Benjamin Brackett: Changed $_SESSION['login'] to $_SESSION['user']
 function check_login () {
-    if(isset($_SESSION['login'] && $_SESSION['login'] != '')) {
+    if(isset($_SESSION['user'] && $_SESSION['user'] != '')) {
        return true;
     } else {
        false;
@@ -27,13 +42,7 @@ function check_login () {
 
 
 <?php
-//Author: Ben Brackett 02/06/16
-//Email: brackeb1@ferris.edu
-//Ben Brackett: I found this code written by David Walsh on
-//https://davidwalsh.name/php-validatie-numeric-digits 
-//Ben Brackett: This function checks for digits only, no dots
-//If it does match a digit then it will return true, if it doesn't
-//match a digit then it will return a false
+
 function is_digits($element) {
 	return !preg_match ("/[^0-9]/", $element);
 }
