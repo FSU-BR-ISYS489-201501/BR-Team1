d62a3424426fa4e28971afb548536c421ec09313
alfadhf <alfadhf@ferris.edu>
2016-02-14 12:57:28 -0500

    Faisal : Edited the header, and changed return value to True/False

--- JCI/includes/CheckEmail.php
+++ JCI/includes/CheckEmail.php
@@ -1,20 +1,31 @@
 <?php
+/*********************************************************************************************
+ * Original Author: Faisal Alfadhli
+ * Date of origination: 02/10/2016
+ *
+ * Page created for use in the JCI Project.
+ * Project work is done as part of a Capstone class ISYS489: Ferris State University.
+ * Purpose: function to be used in multiple pages.
+ * Credit: 1-  www.W3schools.com/php 
+ *
+ * Function:  checkEmail($email)
+ * Purpose: This Function will check the email if it is well formed
+ * Variable: $email - it needs to be verified like this format address@domain.com*
+ * 
+ *
+ * Revision1.1: 02/13/2016 Author: Faisal Alfadhli
+ * Description of change. Chenged the return to True & False instead of 0 & 1.
+ ********************************************************************************************/
 
-/* Faisal Alfadhli 
-refrence: http://www.w3schools.com
-*/
-
-
-		 
-	//This Function will check the email if it is well formed
+	
 	Function checkEmail($email) {
-			
-		$successMsg = 0;
     	//check if email address is well formed
 		if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			$successMsg = 1;
+			return TRUE;
 		}
-		return $successMsg;
+		else {
+			return FALSE;
+		}
   	}
 
 
