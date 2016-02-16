12ff1be832b4ddb6da8c412628938dce4f3b0108
Benjamin Brackett <brackeb1@ferris.edu>
2016-02-15 23:50:12 -0500

    Ben Brackett - fixed header

--- JCI/includes/LoginFunction.php
+++ JCI/includes/LoginFunction.php
@@ -1,10 +1,35 @@
-<?php # Script 12.2 - login_functions.inc.php
-// This page defines two functions used by the login/logout process.
+<?php 
+/*********************************************************************************************
+ * Original Author: Benjamin Brackett
+ * Date of origination: 02/14/2016
+ *
+ * Page created for use in the JCI Project.
+ * Project work is done as part of a Capstone class ISYS489: Ferris State University.
+ * Purpose: login_functions.inc.php. This page defines two functions used by the login/logout process.
+ * Credit: Ullman, Larry (2011-09-13). PHP and MySQL for Dynamic Web Sites, Fourth Edition: Visual QuickPro Guide (4th Edition) 
+ * 			(Kindle Location 30). Pearson Education. Kindle Edition.
+ *
+ * Function:  redirect_user($page)
+ * Purpose: This function determines an absolute URL and redirects the user there.
+ * 				The function takes one argument: the page to be redirected to.
+ * 				The argument defaults to index.php.
+ * Variable: $page - Defines the url for the index page
+ *
+ * Function:  check_login($dbc, $email, $pass)
+ * Purpose: This function validates the form data (the email address and password)
+ * 				If both are present, the database is queried.
+ * 				The function requires a database connection.
+ * 				The function returns an array of information, including:
+ *  			a TRUE/FALSE variable indicating success
+ *  			an array of either errors or the database result
+ * Variable: $dbc - connects to the database
+ * Variable: $email - uses the email that was grabbed in the email field
+ * Variable: $pass - uses the password that was grabbed in the password field
+ *
+ * Revision1.1: MM/DD/YYYY Author: Name Here 
+ * Description of change. Also add //Name: comments above your change within the code.
+ ********************************************************************************************/
 
-/* This function determines an absolute URL and redirects the user there.
- * The function takes one argument: the page to be redirected to.
- * The argument defaults to index.php.
- */
 function redirect_user ($page = 'index.php') {
 
 	// Start defining the URL...
@@ -24,13 +49,7 @@ function redirect_user ($page = 'index.php') {
 } // End of redirect_user() function.
 
 
-/* This function validates the form data (the email address and password).
- * If both are present, the database is queried.
- * The function requires a database connection.
- * The function returns an array of information, including:
- * - a TRUE/FALSE variable indicating success
- * - an array of either errors or the database result
- */
+
 function check_login($dbc, $email = '', $pass = '') {
 
 	$errors = array(); // Initialize error array.
