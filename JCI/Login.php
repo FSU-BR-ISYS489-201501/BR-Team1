210cfa75c380318d38c77534cce5b7c0998fb1c2
Benjamin Brackett <brackeb1@ferris.edu>
2016-03-20 23:35:16 -0400

    Ben Brackett - added more info to Sessions data

--- JCI/Login.php
+++ JCI/Login.php
@@ -12,13 +12,14 @@
  * Revision1.1: MM/DD/YYYY Author: Name Here 
  * Description of change. Also add //Name: comments above your change within the code.
  ********************************************************************************************/
-
+ 
 // Check if the form has been submitted:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 	// Need two helper files:
 	require ('includes/LoginFunction.php');
-	require ('../DbConnector.php');
+	//require ('../DbConnector.php');
+	require ('mysqli_connect.php');
 		
 	// Check the login:
 	list ($check, $data) = checkLoginFields($dbc, $_POST['email'], $_POST['pass']);
@@ -26,9 +27,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if ($check) { // OK!
 		
 		// Set the session data:
+		// Need to add views to database. Does not work now
 		session_start();
-		$_SESSION['USERID'] = $data['USERID'];
-		$_SESSION['Fname'] = $data['Fname'];
+		if(isset($_SESSION['views'])){
+			$_SESSION['views'] = $_SESSION['views']+1;
+			}else{
+				$_SESSION['views']=1;
+		}
+		// User session data:
+		$_SESSION['UserId'] = $data['UserId'];
+		$_SESSION['FName'] = $data['FName'];
+		$_SESSION['LName'] = $data['LName'];
+		$_SESSION['Type'] = $data['Type'];
+		$_SESSION['Email'] = $data['Email'];
 		
 		// Store the HTTP_USER_AGENT:
 		$_SESSION['agent'] = md5($_SERVER['HTTP_USER_AGENT']);
