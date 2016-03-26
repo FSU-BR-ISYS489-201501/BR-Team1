8e73d398c809f1671ce80b62a07fdb4d29b165ce
markbowman100 <markbowman100@gmail.com>
2016-03-25 22:00:39 -0400

    Mark Bowman customized the header depending on account type

--- JCI/includes/Header.php
+++ JCI/includes/Header.php
@@ -7,68 +7,83 @@
 <html>
 <head>
 	<title><?php echo $page_title; ?></title>	
-	<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset='utf-8' />
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
 <title>GRDE328: JCI</title>
 <!--[if lt IE 9]>
-<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<script src='http://html5shim.googlecode.com/svn/trunk/html5.js'></script>
 <![endif]-->
 		
-<link rel="stylesheet" href="styles/jci.css">
+<link rel='stylesheet' href='styles/jci.css'>
 <link href='https://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>
 </head>
-<body>
 	<body>
-	
-	<div class="header">
-		<div class="container">
-	<div class="logo">
-		<img src="styles/images/icon_logo_opt.png" alt="Logo" class="icon">
-		<h3 class="tertiary" id="jci">Journal of Critical Incidents</h3>
-		
-		<div class="login">
-		<a href="Login.php" class="login">LOGIN</a>
-		<a href="Register.php" class="login">REGISTER</a>
-		<a href="Logout.php" class="login">LOGOUT</a>
-		</div>
-	</div>
-	
-
-	
-	<br>
-	<div class="mainmenu">
-		<a href='Index.php' class="button2">HOME</a>
-		<a href='SubmitCase.php' class="button2">SUBMIT A CASE</a>
-		<a href='ContactUs.php' class="button2">CONTACT US</a>
-		<a href='AboutUs.php' class="button2">About Us</a>
-		<a href='Search.php' class="button2">Search</a>
-		
-	</div>
-	
-	
-	<div id="header">
-		
-	</div>
-	<div id="navigation">
-		<ul>
-<!--Ben Brackett: changed login href link 
-<!--Ben Brackett: changed a spelling mistake for logout
-<!--Ben Brackett: changed logout link-->
-<!-- <<<<<<< HEAD -->
-			<li><a href="ManageAnnouncements.php">Manage Announcements</a></li>
-			<li><a href="FileManagement.php">File Management</a></li>
-			<li><a href="BrowseCriticalIncidents.php">Browse Critical Incidents</a></li>
-			<li><a href="CriticalIncidents.php">Critical Incidents</a></li>
-			<!--
-=======
-			<li><a href="index.php">Home Page</a></li>
-			<li><a href="Register.php">Register</a></li>
-			<li><a href="LoginPage.php">Login</a></li>
-			<li><a href="Logout.php">Lougout</a></li>
-			<li><a href="contact.php">Contact Us</a></li>
->>>>>>> ac3072db73b496b2e4cccd03501441faf0b1e589 -->
-		</ul>
-	</div>
-</div>
-</div>
-	<div id="content"><!-- Start of the page-specific content. -->
+		<div class='header'>
+					<div class='container'>
+						<div class='logo'>
+							<img src='styles/images/icon_logo_opt.png' alt='Logo' class='icon'>
+							<h3 class='tertiary' id='jci'>Journal of Critical Incidents</h3>
+							
+		<?php
+			session_start();
+			if(!isset($_SESSION['Type'])) {
+				echo "
+							<div class='login'>
+								<a href='Login.php' class='login'>LOGIN</a>
+								<a href='Register.php' class='login'>REGISTER</a>
+							</div>
+						</div>
+					</div>
+					<br>
+					<div class='mainmenu'>
+						<a href='Index.php' class='button2'>Home</a>
+						<a href='Search.php' class='button2'>Search</a>
+						<a href='AboutUs.php' class='button2'>About Us</a>
+						<a href='ContactUs.php' class='button2'>Contact Us</a>
+					</div>
+				</div>
+				";
+			}
+			else {
+				if ($_SESSION['Type'] == 'Editor' || $_SESSION['Type'] == 'editor') {
+					echo "
+							<div class='login'>
+									<a href='Logout.php' class='login'>LOGOUT</a>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class='mainmenu'>
+							<a href='Index.php' class='button2'>Home</a>
+							<a href='Search.php' class='button2'>Search</a>
+							<a href='AboutUs.php' class='button2'>About Us</a>
+							<a href='ContactUs.php' class='button2'>Contact Us</a>
+							<a href='SubmitCase.php' class='button2'>Submit a Case</a>
+							<a href='BrowseCriticalIncidents.php' class = 'button2'>Browse Critical Incidents</a>
+							<a href='ManageAnnouncements.php' class = 'button2'>Manage Announcements</a>
+							<a href='CriticalIncidents.php' class = 'button2'>Reviewer Management</a>
+						</div>
+					</div>
+					";
+				}
+				else if ($_SESSION['Type'] == 'Author' || $_SESSION['Type'] == 'author') {
+					echo "
+								<div class='login'>
+									<a href='Logout.php' class='login'>LOGOUT</a>
+								</div>
+							</div>
+						</div>
+						<br>
+						<div class='mainmenu'>
+							<a href='Index.php' class='button2'>Home</a>
+							<a href='Search.php' class='button2'>Search</a>
+							<a href='AboutUs.php' class='button2'>About Us</a>
+							<a href='ContactUs.php' class='button2'>Contact Us</a>
+							<a href='SubmitCase.php' class='button2'>Submit a Case</a>
+						</div>
+					</div>
+					";
+				}
+			}
+		?>
+		<div id='content'><!-- Start of the page-specific content. -->
