ed7ec4725c0f80244b464ff601cc43405a2fb9b8
Shane Workman <workma18@gmail.com>
2016-03-31 02:32:58 -0400

    Shane: not quite finished "I think" getting a failed upload, but i think its my file paths.

--- JCI/PdfViewer.php
+++ JCI/PdfViewer.php
@@ -1,3 +1,25 @@
 <?php
-    
+  /*********************************************************************************************
+  * Original Author: Shane Workman
+  * Date of origination: 03/30/2016
+  *
+  * Page created for use in the JCI Project.
+  * Project work is done as part of a Capstone class ISYS489: Ferris State University.
+  * Purpose: The purpose of this page is to allow people to send an email to the editor.
+  * Credit: My own code, with inspiration and use of others functions within the project. http://php.net/ as a resource.
+  *********************************************************************************************/
+  $page_title = 'PDF';
+  require ('../DbConnector.php');
+  include ("includes/Header.php");
+  $selectID = $_SERVER['QUERY_STRING'];
+  $query = "SELECT JournalLocation FROM `journalofcriticalincidents` WHERE '$selectID';";
+  $run = mysqli_query($dbc, $query);
+  $file = '$run';
+  $filename = '$run';
+  header ('Content-type: application/pdf');
+  header ('Content-disposition: inline; filename="' . $filename . '"');
+  header ('Content-Transfer-Encoding: binary');
+  header ('Accept-Range: bytes');
+  @readfile($file);
+  include ("includes/Header.php");
 ?>
