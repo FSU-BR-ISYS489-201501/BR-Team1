af5de043145579a24a66b0846b49dc4ffd42efbf
Shane Workman <workma18@gmail.com>
2016-04-01 00:36:50 -0400

    Shane: Updated pdfviewer... Still not working on my end... looking for some feed back.

--- JCI/PdfViewer.php
+++ JCI/PdfViewer.php
@@ -6,20 +6,25 @@
   * Page created for use in the JCI Project.
   * Project work is done as part of a Capstone class ISYS489: Ferris State University.
   * Purpose: The purpose of this page is to allow people to send an email to the editor.
-  * Credit: My own code, with inspiration and use of others functions within the project. http://php.net/ as a resource.
+  * Credit: My own code, with inspiration and use of others functions within the project. 
+  * http://php.net/ was a resource.
   *********************************************************************************************/
   $page_title = 'PDF';
   require ('../DbConnector.php');
-  include ("includes/Header.php");
+  //include ("includes/Header.php");
+  //include ("includes/TableRowHelper.php");
   $selectID = $_SERVER['QUERY_STRING'];
   $query = "SELECT JournalLocation FROM `journalofcriticalincidents` WHERE '$selectID';";
   $run = mysqli_query($dbc, $query);
-  $file = '$run';
-  $filename = '$run';
-  header ('Content-type: application/pdf');
-  header ('Content-disposition: inline; filename="' . $filename . '"');
-  header ('Content-Transfer-Encoding: binary');
-  header ('Accept-Range: bytes');
-  @readfile($file);
-  include ("includes/Header.php");
+  $file = $run;
+  header('Content-type: application/pdf');
+  header('Content-Disposition: inline');
+  header('Content-Transfer-Encoding: binary');
+  header('Accept-Ranges: bytes');
+  header("Location: $file");
+    @readfile($file);
+ ?>
+<!--
+<?php
+  include ("includes/Footer.php");
 ?>
