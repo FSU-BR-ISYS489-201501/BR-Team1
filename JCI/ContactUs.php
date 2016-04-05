2d369b94faa8f868c250f3cf5d5598ff90fdbd77
Shane Workman <workma18@gmail.com>
2016-04-05 00:26:36 -0400

    Shane: Removed the Table Setup.

--- JCI/ContactUs.php
+++ JCI/ContactUs.php
@@ -13,55 +13,22 @@ include ("includes/Header.php");
 require ('../DbConnector.php');
  ?>
  <h1>Contact Us</h1>
-<form name="contactform" method="post" action="sendFormEmail.php">
-	<table>
-	 	<tr>
-			<td>
-	 			<label for="first_name">First Name *</label>
-			</td>
-			<td >
-				<input  type="text" name="first_name" maxlength="50" size="30">
-			</td>
-		</tr>
-		<tr>
-			<td >
-	 			<label for="last_name">Last Name *</label>
-	 		</td>
-	 		<td >
-	 			<input  type="text" name="last_name" maxlength="50" size="30">
-	 		</td>
-		</tr>
-	 	<tr>
-	 		<td >
-	 			<label for="email">Email Address *</label>
-	 		</td>
-			<td >
-				<input  type="text" name="email" maxlength="80" size="30">
-	 		</td>
-		</tr>
-	 	<tr>
-	 		<td >
-	 			<label for="telephone">Telephone Number</label>
-	 		</td>
-			<td >
-	 			<input  type="text" name="telephone" maxlength="30" size="30">
-	 		</td>
-		</tr>
-		<tr>
-	 		<td >
-	 			<label for="comments">Comments *</label>
-	 		</td>
-	 		<td >
-				<textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
-	 		</td>
-		</tr>
-		<tr>
-	 		<td colspan="2" style="text-align:center">
-				<input type="submit" value="Submit"> 
-	 		</td>
-	 	</tr>
- 	</table>
- </form>
+ <fieldset>
+	<form name="contactform" method="post" action="sendFormEmail.php">
+		<label for="first_name">First Name *</label>
+		<input  type="text" name="first_name" maxlength="50" size="30">
+		<label for="last_name">Last Name *</label>
+		<input  type="text" name="last_name" maxlength="50" size="30">
+		<label for="email">Email Address *</label>
+		<input  type="text" name="email" maxlength="80" size="30">
+		<label for="telephone">Telephone Number</label>
+		<input  type="text" name="telephone" maxlength="30" size="30">
+		<label for="comments">Comments *</label>
+		<textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea><br />
+		<label for="annotate">* Required</label> 
+		<input type="submit" value="Submit"> 
+	 </form>
+ </fieldset>
  
 <?php
 include ('includes/Footer.php');
