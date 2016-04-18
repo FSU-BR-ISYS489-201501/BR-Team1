f4daa4321ecc9d28b52e6bcb6d2964e492b69117
Shane Workman <workma18@gmail.com>
2016-04-18 01:30:00 -0400

    Shane: added credit!

--- JCI/SendFormEmail.php
+++ JCI/SendFormEmail.php
@@ -6,10 +6,11 @@
   * Page created for use in the JCI Project.
   * Project work is done as part of a Capstone class ISYS489: Ferris State University.
   * Purpose: The purpose of this page is to allow people to send an email to the editor.
-  * Credit: partially my own code, from my Final project in ISYS288 - Had help but don't remember the source.
+  * Credit: partially my own code, made with my team from my Final project in ISYS288 
+  * 			we used a source however it is unknown, other than it was on stackoverflow.com/
   *********************************************************************************************/
   $page_title = "Email Confirmation";
-include ('includes/Header.php');
+  include ('includes/Header.php');
 
 if(isset($_POST['email'])) {
 
