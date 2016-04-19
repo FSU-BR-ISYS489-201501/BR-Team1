30538e716f5c9c2fef6c4d9e1bef79081f3477fb
markbowman100 <markbowman100@gmail.com>
2016-04-19 19:44:55 -0400

    Mark Bowman altered code to recognize a single file or an array of files

--- JCI/includes/FileHelper.php
+++ JCI/includes/FileHelper.php
@@ -43,6 +43,11 @@
 	 * 
 	 * Revision 1.5: 04/17/2016 Author: Mark Bowman
 	 * Description of Change: Removed unused function and added/changed comments.
+	 * 
+	 * Revision 1.6: 04/19/2016 Author: Mark Bowman
+	 * Description of Change: I added a way to upload pictures for the website and test if there is only one upload at
+	 * a time.
+	 * 
 	 *******************************************************************************************************************/
 	
 	// This function will upload a file from the host's computer to the server. 
@@ -61,8 +66,19 @@
 		for($i; $i < $numberOfFilesUploaded; $i++) {
 			// This block contains variables for the browser's temporary name
 			// for the uploaded file and the location it is going to be saved to.
-			$tempUploadedFileName = $_FILES["$htmlElement"]['tmp_name'][$i];
-			$uploadedFileNameSaveLocation = $fileStorageLocation . "{$_FILES["$htmlElement"]['name']["$i"]}";
+			if (is_array($_FILES["$htmlElement"]['tmp_name'])) {
+				$tempUploadedFileName = $_FILES["$htmlElement"]['tmp_name'][$i];
+			}
+			else {
+				$tempUploadedFileName = $_FILES["$htmlElement"]['tmp_name'];
+			}
+
+			if (is_array($_FILES["$htmlElement"]['name'])) {
+				$uploadedFileNameSaveLocation = $fileStorageLocation . "{$_FILES["$htmlElement"]['name']["$i"]}";
+			}
+			else {
+				$uploadedFileNameSaveLocation = $fileStorageLocation . "{$_FILES["$htmlElement"]['name']}";
+			}
 			
 			$fileName = $_FILES[$htmlElement]['name'][$i];
 			$id = $ids[$i];
@@ -82,12 +98,19 @@
 				$stmt = mysqli_prepare($dbc, $insertFileLocationSqlQuery);
 				mysqli_stmt_bind_param($stmt, 'isss', $journalId, $uploadedFileNameSaveLocation, $type, $fileName);
 			}
+			else if ($types[$i] == 'About' || $types[$i] == 'Slide') {
+				// I added a way for pictures to be uploaded.
+				$insertFileLocationSqlQuery = "INSERT INTO files (FileLocation, FileType, FileDes)
+					VALUES (?, ?, ?)";
+				$stmt = mysqli_prepare($dbc, $insertFileLocationSqlQuery);
+				mysqli_stmt_bind_param($stmt, 'sss', $uploadedFileNameSaveLocation, $type, $fileName);
+			}
 			
 			// This block checks if a file has been submitted with the HTML form.
 			if(file_exists($tempUploadedFileName)) {
 				// This block performs an SQL query to insert file
 				// location into the database.
-				if (mysqli_stmt_execute($stmt)or die("Errors are ".mysqli_error($dbc))) {
+				if (mysqli_stmt_execute($stmt)) {
 					mysqli_stmt_close($stmt);
 					// This block moves the input file to the file server.
 					if(move_uploaded_file($tempUploadedFileName, $uploadedFileNameSaveLocation)) {
